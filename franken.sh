a9cb83780ca3857de2d7e0f60f1c3563a01e7138
acuicultor <acuicultor@users.noreply.github.com>
2020-07-11 11:45:51 +0200

    techpack: display: add OnePlus 8 missing config entries
    Adding CONFIG_PXLW_SOFT_IRIS=y fixes the black screen on calls for OnePlus8 devices

--- franken.sh
+++ franken.sh
@@ -12,16 +12,16 @@ clear
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image"
 DTBIMAGE="dtb"
-export CLANG_PATH=~/android/clang/clang-r383902/bin/
+export CLANG_PATH=~/android/clang/clang-r383902c/bin/
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=${HOME}/android/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export LD_LIBRARY_PATH=${HOME}/android/clang/clang-r383902/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${HOME}/android/clang/clang-r383902c/lib64:$LD_LIBRARY_PATH
 DEFCONFIG="franken_defconfig"
 
 # Kernel Details
-VER=".p6.2"
+VER=".p11.2"
 
 # Paths
 KERNEL_DIR=`pwd`
